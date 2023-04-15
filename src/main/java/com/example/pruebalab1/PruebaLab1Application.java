96acc48b2396747ad967dd6a9c018aa14c9e3dd4
matiox23 <matias_ramirez@hotmail.com>
2023-04-15 17:14:27 -0500

    first commit

---
+++ src/main/java/com/example/pruebalab1/PruebaLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.pruebalab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pruebalab1/PruebaLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebalab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
